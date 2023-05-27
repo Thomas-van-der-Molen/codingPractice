186453b2de41bbe5b75c26918dec74477ae50abd
Thomas <58376739+Thomas-van-der-Molen@users.noreply.github.com>
2023-05-27 18:57:17 -0400

    Practice leetcode contest 105

---
+++ contest105.py
@@ -0,0 +1,27 @@
+
+#buy two chocolates
+def buyChoco(prices, money):
+        prices.sort()
+        if prices[0] + prices[1] <= money:
+            return money - (prices[0] + prices[1])
+        else:
+            return money
+
+#maximum strength of a group
+
+def maxStrength(nums):
+    if max(nums) <= 0: return max(nums)
+    strength = 1
+    nums.sort()
+    for index, value in enumerate(nums):
+        if value < 0 and index < len(nums) -1 and nums[index + 1] < 0:
+            strength *= value
+        elif value > 0:
+            strength *= value
+    return strength
+
+nums = [3,-1,-5,2,5,-9]
+print(maxStrength(nums))
+
+nums = [-4,-5,-4]
+print(maxStrength(nums))
